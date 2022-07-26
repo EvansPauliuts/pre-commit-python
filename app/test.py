79601500ed616e268c365041e401fda705c0c086
Evans Pauliuts <pauliutsevans@gmail.com>
2022-07-26 16:10:27 +0300

    :rocket: fix(work): Update pre-commit #1

--- app/test.py
+++ app/test.py
@@ -1,13 +1,11 @@
-def greet(name: str):
-        print(f"hello, {name}")
+def greet(name : str):
+        print(f'hello, {name}')
     print("nice to meet you!")
-
 
 def main():
     for name in "James", "Subscriber", "other" \
             :
         greet(name)
-
 
 if __name__ == "__main__":
     main()
